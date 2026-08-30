55dc63124f6580fab30c84a555a5ae0fa8b4881a
James Hodgkinson <james@terminaloutcomes.com>
2026-08-30 19:38:49 +1000

    Keep transcription open during playback

--- App/RootView.swift
+++ App/RootView.swift
@@ -1579,9 +1579,9 @@ private struct SignedInView: View {
     #else
         @ViewBuilder
         private func mobileTabs(containerHeight: CGFloat) -> some View {
-            if model.playback.showsMiniPlayer {
-                mobileTabView(containerHeight: containerHeight)
-                    .tabViewBottomAccessory {
+            mobileTabView(containerHeight: containerHeight)
+                .tabViewBottomAccessory {
+                    if model.playback.showsMiniPlayer {
                         MiniPlayerView(
                             playback: model.playback,
                             containerHeight: containerHeight
@@ -1589,9 +1589,7 @@ private struct SignedInView: View {
                             navigation.showsPlayer = true
                         }
                     }
-            } else {
-                mobileTabView(containerHeight: containerHeight)
-            }
+                }
         }
 
         private func mobileTabView(containerHeight: CGFloat) -> some View {
